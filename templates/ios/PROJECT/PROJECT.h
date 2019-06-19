da583bfddb3dc3fdd76d14558c749f91d00c3f93
Qiongpan Ke <keqiongpan@163.com>
2019-06-19 15:05:43 +0800

    [新增] 新增针对iOS平台使用Objective-C语言的构建framework的工程。

---
+++ templates/ios/PROJECT/PROJECT.h
@@ -0,0 +1,19 @@
+//
+//  PROJECT.h
+//  PROJECT
+//
+//  Created by keqiongpan@163.com on 2019/6/19.
+//  Copyright © 2019 PROJECT_OWNER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PROJECT.
+FOUNDATION_EXPORT double PROJECTVersionNumber;
+
+//! Project version string for PROJECT.
+FOUNDATION_EXPORT const unsigned char PROJECTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PROJECT/PublicHeader.h>
+
+
